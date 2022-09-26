29a8ea563403f8715527a91add7aef4b6375e547
JangGwon <tlsakch510@naver.com>
2022-09-26 17:25:53 +0900

    Main.java 수정 #4

--- Main.java
+++ Main.java
@@ -2,8 +2,14 @@ package studentA
 
 public class Main
 {
+	static void say()
+	{
+		System.out.println("main");
+	
+	}
 	public static void main(String[] args)
 	{
+		say();
 		Hello hello = new Hello();
 		hello.say("hello world");
 	}
